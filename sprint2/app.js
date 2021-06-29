b287e99c980b149d1b3e3f4d08f4e2677cdfc532
EnzoAguero <agueroenzo10@gmail.com>
2021-06-29 01:14:12 -0300

    agrego parte de la rama enzo

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,5 +6,7 @@ const port = 3050;
 
 app.listen(port, () => console.log('Server running as ' + port))
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/index', (req,res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+
 
