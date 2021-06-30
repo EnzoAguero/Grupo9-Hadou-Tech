d7fbedbccd7327e38685669d9edb5e7ded5dba5b
Mati-G <Matiasn.gimenez@hotmail.com>
2021-06-30 14:26:18 -0300

    Agregado register

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,7 +6,8 @@ const port = 3050;
 
 app.listen(port, () => console.log('Server running as ' + port))
 
-app.get('/index', (req,res) => res.sendFile(path.join(__dirname, 'index.html')))
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get('/index', (req,res) => res.sendFile(path.join(__dirname, 'viwes','index.html')))
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 
 
