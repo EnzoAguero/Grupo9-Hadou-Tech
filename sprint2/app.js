fde99dcb7a87454b88d82403caa5e2631e559af3
Mati-G <Matiasn.gimenez@hotmail.com>
2021-06-29 22:32:54 -0300

    Agregando fondo

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,5 +6,5 @@ const port = 3050;
 
 app.listen(port, () => console.log('Server running as ' + port))
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 
