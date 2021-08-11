3418633ef08b3422b3ffbb3ebee51dbcf7c6f994
EnzoAguero <agueroenzo10@gmail.com>
2021-08-11 15:57:29 -0300

    cambios en detalle, arreglo de css

--- site/src/data/products.js
+++ site/src/data/products.js
@@ -6,4 +6,3 @@ module.exports = {
     productos : JSON.parse(fs.readFileSync(path.join(__dirname,'products.json'),'utf-8')),
     guardar : data => fs.writeFileSync(path.join(__dirname,'products.json'),JSON.stringify(data,null,2),'utf-8'),
 } 
-
