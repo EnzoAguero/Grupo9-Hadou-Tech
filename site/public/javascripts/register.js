3283861b7518eb08bd98f1a16b8c650a4e044a0b
EnzoAguero <agueroenzo10@gmail.com>
2021-10-18 16:18:02 -0300

    agrego middlewares, validaciones front

---
+++ site/public/javascripts/register.js
@@ -0,0 +1,53 @@
+const $ = id => document.getElementById(id)
+
+window.addEventListener('load', () => {
+    console.log('Conectado bien!');
+
+const form = $('formRegister');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+})
+
+$('email').addEventListener('blur', () => {
+    if(!$('email').value){
+        $('email').classList.add('is-invalid')
+            $('error-email').innerHTML = "Debes ingresar un email válido"
+    }else{
+        $('email').classList.remove('is-invalid')
+        $('email').classList.add('is-valid')
+        $('error-email').innerHTML = null
+    }
+}),
+$('password').addEventListener('blur', () => {
+    if(!$('password').value){
+        $('password').classList.add('is-invalid')
+            $('error-password').innerHTML = "Debes ingresar una contraseña válida"
+    }else{
+        $('password').classList.remove('is-invalid')
+        $('password').classList.add('is-valid')
+        $('error-password').innerHTML = null
+    }
+}),
+$('name').addEventListener('blur', () => {
+    if(!$('name').value){
+        $('name').classList.add('is-invalid')
+            $('error-name').innerHTML = "Debes ingresar un nombre válido"
+    }else{
+        $('name').classList.remove('is-invalid')
+        $('name').classList.add('is-valid')
+        $('error-name').innerHTML = null
+    }
+}),
+$('last_name').addEventListener('blur', () => {
+    if(!$('last_name').value){
+        $('last_name').classList.add('is-invalid')
+            $('error-last_name').innerHTML = "Debes ingresar un apellido válido"
+    }else{
+        $('last_name').classList.remove('is-invalid')
+        $('last_name').classList.add('is-valid')
+        $('error-last_name').innerHTML = null
+    }
+})
+
+}) 
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 const validaciones = require('../middlewares/addProductValidation');
 const upload = require('../middlewares/multer')
+const adminCheck = require('../middlewares/adminCheck')
 
 const {add,detail,edit,update,remove,save,search} = require('../controllers/productsController');
 
 
 
 /* GET home page. */
-router.get('/add', add) /* formulario */
+router.get('/add',adminCheck, add) /* formulario */
 router.post('/add', upload.single('image'),validaciones, save); 
 
 router.get('/detalle/:id',detail); /*   */
 
-router.get('/edit/:id',edit); 
+router.get('/edit/:id',adminCheck,edit); 
 router.put('/edit/:id',update); 
 
 router.get('/search',search);
