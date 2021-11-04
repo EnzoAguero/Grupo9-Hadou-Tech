252b5fb0fca65cd3c7a048b8fff8828cc548f0b2
EnzoAguero <agueroenzo10@gmail.com>
2021-11-04 10:34:50 -0300

    .

--- site/database/config/config.js
+++ site/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Independiente22",
+    "password": "carlita14",
     
     "database": "hadou_tech",
     "host": "127.0.0.1",
